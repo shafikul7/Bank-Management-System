5fcf355be3b2ce082c70232903dd8cb467298bca
mdshafikulislam <shafikul1170@gmail.com>
2022-10-27 15:50:01 +0600

    project test

--- Bank Management System in C++/bank-management.cpp
+++ Bank Management System in C++/bank-management.cpp
@@ -1,3 +1,7 @@
+//***************************************************************
+//    			STARTING A PROJECT
+//***************************************************************
+
 #include<iostream>
 #include <ctime>
 #include<fstream>
@@ -15,8 +19,25 @@ CREATED BY       : MD SHAFIKUL ISLAM
 LANGUAGE         : C++
 DATE OF CREATION : 22/10/2022
 PLATFORM         : WINDOWS
+SOURCE CODE		 : https://github.com/mdshafikulislam/Bank-Management-System
 TESTED ON        : WINDOWS 10
-ABOUT PROJECT    : Bank Account Management System is a application that helps to keep track of all the customer records .
+ABOUT PROJECT    : The Bank Management System an application for keeping up
+					an individual's record in a bank. In this basic project I attempted
+					to show the working of banking system and spread theessential
+					usefulness of a Bank Management System. It is an application
+					that is used to maintain a customer’s account in bank. The main
+					aim of this project is to develop software for
+					BankManagementSystem. This project has been developed to
+					carry out the processes easily and quickly, which is not possible
+					with the manual systems, which are overcome by this software.
+					Our Banking Management System will give relief to both the
+					customer and employees as well. This system will be used to
+					store customer data, their cash and information as well and will
+					do transaction upon requesting. It will also keep record of each
+					transaction. We can also perform other tasks as well. For
+					example; we can add customer, we can update their record,
+					delete a customer, withdraw and transact amount and keep a
+					record of each transaction.
 */
 
 
@@ -273,7 +294,6 @@ main()
 	char ch;
 	int num;
 
-	//add
 
 	int choice;
         time_t now = time(0);
@@ -312,8 +332,6 @@ main()
                         main();
         }
 
-
-	//end
 
 
 	do
@@ -492,7 +510,7 @@ void Updating_Bank_Account(int n)
 			cout<<"\n\n\tPlease Enter The New Details of Bank_Account"<<endl;
 			ac.Updation();
 			int pos=(-1)*static_cast<int>(sizeof(Bank_Account));
-			File.seekp(pos,ios::cur); //fncallat1353
+			File.seekp(pos,ios::cur);
 		    File.write(reinterpret_cast<char *> (&ac), sizeof(Bank_Account));
 		    cout<<"\n\n\tRecord Updated";
 		    found=true;
